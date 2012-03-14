4629c8c8227021a2f2e18e11d2acd0feb792903f
Evgeny Zislis <evgeny.zislis@gmail.com>
2012-03-14 10:11:14 +0200

    move url opener class ioc into read method

--- amazon_query.py
+++ amazon_query.py
@@ -11,11 +11,11 @@ class AmazonQuery(object):
     :type endpoint: string
     :param endpoint: http://docs.amazonwebservices.com/general/latest/gr/rande.html
 
-    :type access_key_id: string
-    :param access_key_id: The Access Key ID for the request sender.
+    :type key_id: string
+    :param key_id: The Access Key ID for the request sender.
 
-    :type secret_access_key: string
-    :param secret_access_key: Secret Access Key used for request signature.
+    :type secret_key: string
+    :param secret_key: Secret Access Key used for request signature.
 
     :type action: string
     :param action: Indicates the action to perform.
@@ -24,28 +24,25 @@ class AmazonQuery(object):
     :param parameters: Optional additional request parameters.
     """
 
-    def __init__(self, endpoint, access_key_id, secret_access_key,
-                 action, parameters={}, opener_class=None):
+    def __init__(self, endpoint, key_id, secret_key, action, parameters={}):
       # http://docs.amazonwebservices.com/AWSEC2/latest/APIReference/Query-Common-Parameters.html
-      self.opener_class = opener_class or urllib.FancyURLopener
       self.endpoint = endpoint
       parsed = urlparse.urlparse(endpoint)
       self.host = parsed.hostname
       self.path = parsed.path or '/'
-      self.secret_access_key = secret_access_key
+      self.secret_key = secret_key
       self.parameters = {
           'Action': action,
           'Version': '2012-03-01',
-          'AWSAccessKeyId': access_key_id,
+          'AWSAccessKeyId': key_id,
           'SignatureVersion': 2,
           'SignatureMethod': 'HmacSHA256',
       }
       self.parameters.update( parameters )
 
-    def read(self):
+    def read(self, opener_class=urllib.FancyURLopener):
         self.parameters['Timestamp'] = datetime.datetime.utcnow().isoformat()
-        opener = self.opener_class()
-        return opener.open(self.endpoint, self.encoded_parameters)
+        return opener_class().open(self.endpoint, self.encoded_parameters)
 
     @property
     def encoded_parameters(self):
@@ -56,5 +53,5 @@ class AmazonQuery(object):
     def signature(self):
         params = urllib.urlencode( sorted(self.parameters.items()) )
         text = "\n".join(['POST', self.host, self.path, params])
-        auth = hmac.new(self.secret_access_key, msg=text, digestmod=hashlib.sha256)
+        auth = hmac.new(self.secret_key, msg=text, digestmod=hashlib.sha256)
         return base64.b64encode(auth.digest())
