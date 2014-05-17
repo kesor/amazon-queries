67c2ab52a7738f854ee450000b9928af154a7699
Evgeny Zislis <evgeny.zislis@gmail.com>
2014-05-17 18:51:23 +0300

    add a ruby version

---
+++ amazon/query.rb
@@ -0,0 +1,39 @@
+require 'uri'
+require 'time'
+require 'base64'
+require 'net/http'
+require 'digest/hmac'
+
+class AmazonQuery
+
+  def initialize(endpoint, key_id, secret_key, parameters = {})
+    @uri         = URI.parse(endpoint)
+    @host        = @uri.host
+    @port        = @uri.port or 443
+    @path        = @uri.path.empty? ? '/' : @uri.path
+    @secret_key  = secret_key
+    @parameters  = parameters.merge({
+      'AWSAccessKeyId'   => key_id,
+      'SignatureVersion' => 2,
+      'SignatureMethod'  => 'HmacSHA256',
+      'Timestamp'        => Time.now.utc.iso8601
+    })
+  end
+
+  def signed_parameters
+    sorted_params = @parameters.sort_by { |k,v| k.to_s }
+    params = URI.encode_www_form(sorted_params)
+    text = ['POST', @host, @path, params].join("\n")
+    auth = Digest::HMAC.digest(text, @secret_key, Digest::SHA256)
+    signature = Base64.strict_encode64(auth)
+    @parameters.merge!({ 'Signature' => signature })
+  end
+
+  def request
+    http = Net::HTTP.new(@host, @uri.port)
+    http.set_debug_output($stderr) if $DEBUG
+    http.use_ssl = (@port == 443)
+    http.post(@path, URI.encode_www_form(signed_parameters)).body
+  end
+
+end
---
+++ main.rb
@@ -0,0 +1,72 @@
+$: << '.'
+require 'amazon/query'
+
+key_id  = 'AKIAIEXAMPLEEXAMPLE6'
+secret  = 'example+example+example+example+example7'
+
+endpoint = 'http://ec2.us-west-1.amazonaws.com'
+question = { Version: '2014-02-01', Action: 'DescribeInstances' }
+
+puts AmazonQuery.new(endpoint, key_id, secret, question).request
+
+#
+# other questions to try ...
+#
+
+#   endpoint = 'https://cloudformation.us-west-1.amazonaws.com'
+#   question = { Version: '2010-05-15', Action: 'ListStacks' }
+
+#   endpoint = 'http://monitoring.us-west-1.amazonaws.com'
+#   question = { Version: '2010-08-01', Action: 'ListMetrics' }
+
+#   endpoint = 'http://ec2.us-west-1.amazonaws.com'
+#   question = { Version: '2012-03-01', Action: 'DescribeInstances' }
+#   question = { Version: '2012-03-01', Action: 'DescribeReservedInstancesOfferings' }
+#   question = { Version: '2012-03-01', Action: 'DescribeAvailabilityZones' }
+
+#   endpoint = 'http://elasticmapreduce.us-west-1.amazonaws.com'
+#   question = { Version: '2009-03-31', Action: 'DescribeJobFlows' }
+
+#   endpoint = 'http://ec2.us-east-1.amazonaws.com'
+#   question = { Version: '2012-10-01', Action: 'DescribeSpotPriceHistory' }
+
+#   endpoint = 'https://elasticache.us-west-1.amazonaws.com'
+#   question = { Version: '2011-07-15', Action: 'DescribeCacheClusters' }
+
+#   endpoint = 'https://rds.us-west-1.amazonaws.com'
+#   question = { Version: '2012-01-15', Action: 'DescribeDBEngineVersions' }
+
+#   endpoint = 'http://sns.us-west-1.amazonaws.com'
+#   question = { Version: '2010-03-31', Action: 'ListTopics' }
+
+#   endpoint = 'http://sqs.us-west-1.amazonaws.com'
+#   question = { Version: '2011-10-01', Action: 'ListQueues' }
+
+#   endpoint = 'http://sdb.us-west-1.amazonaws.com'
+#   question = { Version: '2009-04-15', Action: 'ListDomains' }
+
+#   endpoint = 'http://s3-us-west-1.amazonaws.com'
+#   question = { Version: '2006-03-01', Action: 'ListAllMyBuckets' }
+
+#   endpoint = 'https://elasticbeanstalk.us-east-1.amazonaws.com'
+#   question = { Version: '2010-12-01', Action: 'ListAvailableSolutionStacks' }
+
+#   endpoint = 'https://iam.amazonaws.com'
+#   question = { Version: '2010-05-08', Action: 'ListUsers' }
+
+#   endpoint = 'http://autoscaling.us-west-1.amazonaws.com'
+#   question = { Version: '2011-01-01', Action: 'DescribeTags' }
+
+#   endpoint = 'https://importexport.amazonaws.com'
+#   question = { Version: '2010-06-01', Action: 'ListJobs' }
+
+#   endpoint = 'https://sts.amazonaws.com'
+#   question = { Version: '2011-06-15', Action: 'GetSessionToken' }
+
+#   endpoint = 'https://iam.amazonaws.com'
+#   question = { Version: '2010-05-08', Action: 'GetUser' }
+#   question = { Version: '2010-05-08', Action: 'ListUserPolicies', UserName: 'test_iam' }
+#   question = { Version: '2010-05-08', Action: 'GetUserPolicy', UserName: 'test_iam', PolicyName: 'ReadOnlyAccess-test_iam-201203291722' }
+
+#   endpoint = 'http://elasticloadbalancing.us-west-1.amazonaws.com'
+#   question = { Version: '2011-11-15', Action: 'DescribeLoadBalancers' }
